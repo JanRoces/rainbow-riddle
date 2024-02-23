2c1dbe57bf7432c7fb07ae4909197a0902a8092a
JanRoces <55074172+JanRoces@users.noreply.github.com>
2024-02-22 23:00:24 -0800

    create logo

---
+++ src/utils/colors.js
@@ -0,0 +1,32 @@
+const COLOR = {
+  RED: {
+    name: 'red',
+    hex: '#FF0000',
+  },
+  ORANGE: {
+    name: 'orange',
+    hex: '#FFA500',
+  },
+  YELLOW: {
+    name: 'yellow',
+    hex: '#FFFF00',
+  },
+  GREEN: {
+    name: 'green',
+    hex: '#008000',
+  },
+  BLUE: {
+    name: 'blue',
+    hex: '#0000FF',
+  },
+  INDIGO: {
+    name: 'indigo',
+    hex: '#4B0082',
+  },
+  VIOLET: {
+    name: 'violet',
+    hex: '#EE82EE',
+  },
+};
+
+export const COLORS = Object.values(COLOR);
