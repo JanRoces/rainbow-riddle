3f64fe7f55697792bed3dcb40c2f6f6bff6d44d1
JanRoces <55074172+JanRoces@users.noreply.github.com>
2024-02-23 18:35:17 -0800

    create game grid

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Logo from './components/Logo';
 import ColorSelection from './components/ColorSelection';
 import './App.css';
+import GameGrid from './components/GameGrid';
 
 function App() {
   return (
     <div>
       <Logo />
+      <GameGrid />
       <ColorSelection />
     </div>
   );
---
+++ src/components/GameGrid.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import '../styles/GameGrid.css';
+
+function GameGrid() {
+  const rowSize = 8;
+  const columnSize = 5;
+
+  function renderColumns(rowIndex) {
+    const columns = [];
+
+    for (let i = 0; i < columnSize; i++) {
+      const key = 'row-' + rowIndex + 'column-' + i;
+
+      columns.push(<div key={key} className="color-square"></div>);
+    }
+
+    columns.push(<div key={'checker-' + rowIndex} className="checker"></div>);
+
+    return columns;
+  }
+
+  function renderRows() {
+    const rows = [];
+
+    for (let i = 0; i < rowSize; i++) {
+      const key = 'row-' + i;
+      const rowIndex = i;
+
+      rows.push(
+        <div key={key} className="grid-row">
+          {renderColumns(rowIndex)}
+        </div>
+      );
+    }
+
+    return rows;
+  }
+
+  return <div>{renderRows()}</div>;
+}
+
+export default GameGrid;
