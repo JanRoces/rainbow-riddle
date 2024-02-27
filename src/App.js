fad0c933e0a96745ec23a53ecb6a7ef87479adcf
JanRoces <55074172+JanRoces@users.noreply.github.com>
2024-02-26 19:21:31 -0800

    generate secret color combination

--- src/App.js
+++ src/App.js
@@ -3,6 +3,23 @@ import Logo from './components/Logo';
 import ColorSelection from './components/ColorSelection';
 import GameGrid from './components/GameGrid';
 import './App.css';
+import { COLORS } from './utils/colors';
+
+const secret = getSecretCombination();
+
+function getSecretCombination() {
+  const maxInputLenth = 5;
+  const secretColors = [];
+
+  for (let i = 0; i < maxInputLenth; i++) {
+    const randomIndex = Math.floor(Math.random() * COLORS.length);
+    const randomColor = COLORS[randomIndex];
+
+    secretColors.push(randomColor);
+  }
+
+  return secretColors;
+}
 
 function App() {
   const [input, setInput] = useState([]);
