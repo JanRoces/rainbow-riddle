4d1cd59dcc11caff539234d11b6db7c0af9533ae
Jan Roces <jroces@chirotouch.com>
2025-06-20 22:58:08 -0700

    add disabled

--- src/components/ActionButton.js
+++ src/components/ActionButton.js
@@ -1,17 +1,22 @@
 import { useMediaQuery } from '@mui/material';
 import React from 'react';
 
-function ActionButton({ label, type, onClick }) {
+function ActionButton({ label, type, onClick, disabled }) {
   const isMobile = useMediaQuery('(max-width:768px)');
 
   function handleOnClick() {
     return onClick();
   }
 
+  function getClassName() {
+    return `button-action-${type}${isMobile ? ' button-mobile' : ''}${disabled ? ' disabled' : ''}`;
+  }
+
   return (
     <button
-      className={`button-action-${type}${isMobile ? ' button-mobile' : ''}`}
+      className={getClassName()}
       onClick={() => handleOnClick()}
+      disabled={disabled}
     >
       {label}
     </button>
