134ea07695ffb19836e107fac23b4cdc0227312c
Jan Roces <jroces@chirotouch.com>
2025-06-20 22:20:01 -0700

    refactor action button

--- src/components/ActionButton.js
+++ src/components/ActionButton.js
@@ -1,29 +1,11 @@
 import { useMediaQuery } from '@mui/material';
 import React from 'react';
 
-function ActionButton({
-  label,
-  type,
-  onDeleteColor,
-  onEnterInput,
-  onPlayAgain,
-  onPlay,
-}) {
+function ActionButton({ label, type, onClick }) {
   const isMobile = useMediaQuery('(max-width:768px)');
 
   function handleOnClick() {
-    switch (type) {
-      case 'delete':
-        return onDeleteColor();
-      case 'enter':
-        return onEnterInput();
-      case 'play-again':
-        return onPlayAgain();
-      case 'play':
-        return onPlay();
-      default:
-        break;
-    }
+    return onClick();
   }
 
   return (
