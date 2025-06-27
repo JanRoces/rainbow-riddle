a221f266fda0f6be5a47e04ab80bbb2460432111
Jan Roces <jroces@chirotouch.com>
2025-06-26 20:47:50 -0700

    render animated squares

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,39 +2,18 @@ import React from 'react';
 import '../styles/LandingPage.css';
 import { COLORS_VIBRANT } from '../utils/colors';
 import ActionButton from './ActionButton';
+import AnimatedSquares from './AnimatedSquares';
 
 function LandingPage({ onPlay }) {
   function renderTitle() {
     return <span>RainbowRiddle</span>;
   }
 
-  function renderSquares() {
-    const squares = [];
-    const bounceDuration = 0.6;
-    const colorsLength = COLORS_VIBRANT.length;
-    const animationDelay = bounceDuration / colorsLength;
-
-    COLORS_VIBRANT.forEach((color, index) => {
-      squares.push(
-        <div
-          key={index}
-          className="square"
-          style={{
-            backgroundColor: color.hex,
-            animationDelay: `${index * animationDelay}s`,
-          }}
-        ></div>,
-      );
-    });
-
-    return squares;
-  }
-
   return (
     <div className="container-landing-page">
       <div className="welcome-text">Welcome to</div>
       <div className="logo">{renderTitle()}</div>
-      <div className="container-squares">{renderSquares()}</div>
+      <AnimatedSquares colors={COLORS_VIBRANT} />
       <ActionButton label="Play" type="play" onClick={onPlay} />
     </div>
   );
