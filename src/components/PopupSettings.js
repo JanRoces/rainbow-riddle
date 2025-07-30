f3790ed261780019bbc9180e7e6dd0d9bbf847dd
JanRoces <rocesjg@gmail.com>
2025-07-29 20:53:10 -0700

    add color choices

--- src/components/PopupSettings.js
+++ src/components/PopupSettings.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Popup, { POPUP_TYPE } from './Popup';
 import '../styles/Popup.css';
+import AnimatedSquares from './AnimatedSquares';
+import { COLORS_VIBRANT, COLORS_PASTEL } from '../utils/colors';
 
 function PopupSettings({ showSettings, toggleShowSettings }) {
   function togglePopup() {
@@ -8,7 +10,28 @@ function PopupSettings({ showSettings, toggleShowSettings }) {
   }
 
   function renderContent() {
-    return <div>Settings Popup</div>;
+    return (
+      <div>
+        <div className="container-toggle-group">
+          <div className="container-toggle-text-and-squares">
+            <div className="container-toggle-text">Vibrant</div>
+            <AnimatedSquares colors={COLORS_VIBRANT} />
+          </div>
+          <div className="container-toggle-icon">
+            <i className="fa-solid fa-toggle-on"></i>
+          </div>
+        </div>
+        <div className="container-toggle-group">
+          <div className="container-toggle-text-and-squares">
+            <div className="container-toggle-text">Pastel</div>
+            <AnimatedSquares colors={COLORS_PASTEL} />
+          </div>
+          <div className="container-toggle-icon">
+            <i className="fa-solid fa-toggle-off"></i>
+          </div>
+        </div>
+      </div>
+    );
   }
 
   return (
